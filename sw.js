6d2256ebf9a7cd22587696dead32d03dd9dfda60
André Luiz Alves <andreluiz1902@gmail.com>
2025-07-27 18:27:24 -0300

    Arquivos

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const CACHE_NAME = 'falaou-paga-v1';
 const assets = [
   '/',
@@ -20,3 +21,27 @@ self.addEventListener('fetch', (event) => {
     caches.match(event.request).then((resp) => resp || fetch(event.request)),
   );
 });
+=======
+const CACHE_NAME = 'falaou-paga-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  // inclua aqui os caminhos das imagens das cartas
+];
+
+// Ao instalar, abre cache e adiciona recursos
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)),
+  );
+});
+
+// Intercepta requisições para servir do cache quando disponível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => resp || fetch(event.request)),
+  );
+});
+>>>>>>> 791001b (Arquivos)
